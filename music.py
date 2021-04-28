35722ca669f987fd5f6ed77287a842d0e1d3bc06
thewallacems <thewallacems@gmail.com>
2021-04-28 17:20:34 -0400

    added Track class with state and details props to clean up the update method in rpc.py

--- music.py
+++ music.py
@@ -1,5 +1,6 @@
 import re
 import subprocess
+from typing import Optional
 
 
 _TRACK_REGEX = r'trackName:(.+), trackArtist:(.+), trackPosition:([0-9]+), trackDuration:([0-9]+)'
@@ -7,7 +8,27 @@ _TRACK_SCRIPT_ARGS = ['osascript', './Resources/getTrack.scpt']
 _TRACK_KEYS = ['trackName', 'trackArtist', 'trackPosition', 'trackDuration']
 
 
-def get_current_track():
+class Track:
+    def __init__(self, trackName, trackArtist, trackPosition, trackDuration):
+        self.trackName = trackName
+        self.trackArtist = trackArtist
+
+        self.trackPosition = int(trackPosition)
+        self.trackDuration = int(trackDuration)
+
+    @property
+    def state(self) -> str:
+        position_minutes, position_seconds = divmod(self.trackPosition, 60)
+        duration_minutes, duration_seconds = divmod(self.trackDuration, 60)
+        state = '%02d:%02d/%02d:%02d' % (position_minutes, position_seconds, duration_minutes, duration_seconds)
+        return state
+
+    @property
+    def details(self) -> str:
+        return f'{self.trackArtist} - {self.trackName}'
+
+
+def get_current_track() -> Optional[Track]:
     result = subprocess.run(_TRACK_SCRIPT_ARGS, capture_output=True)
     if not result.stdout:
         return None
@@ -18,4 +39,4 @@ def get_current_track():
         return None
 
     track = {key: value for key, value in zip(_TRACK_KEYS, match.groups())}
-    return track
+    return Track(**track)
