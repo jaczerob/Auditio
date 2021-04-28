cfc0eaa3f75be8c59bedb36f92f1895ceddf200f
thewallacems <thewallacems@gmail.com>
2021-04-28 17:19:33 -0400

    changed RPC from class to separate methods

--- rpc.py
+++ rpc.py
@@ -1,49 +1,61 @@
-import configparser
+from typing import Optional
+
+import config
+import logger
+import music
 
 from pypresence import Presence
 
 
-config = configparser.ConfigParser()
-config.read('./Resources/config.ini')
+_CONNECTED = False
+_RPC: Optional[Presence] = None
 
 
-class RPC:
-    def __init__(self):
-        self._connected = False
-        self._rpc = None
+def update():
+    if not is_connected():
+        return
 
-    @property
-    def is_connected(self):
-        return self._connected
+    track = music.get_current_track()
 
-    def connect(self):
-        client_id = config.get('RPC', 'clientid', fallback=None)
-        if not client_id:
-            raise KeyError('No clientid set.')
+    if track:
+        logger.info(f'Got Track(name={track.trackName}, artist={track.trackArtist}, position={track.trackPosition}, duration={track.trackDuration})')
+        _RPC.update(state=track.state, details=track.details, large_image='icon', large_text=track.details)
+        logger.info(f'Updated RPC(state={track.state}, details={track.details}, large_image=icon, large_text={track.details})')
+    else:
+        logger.info('No track found, clearing RPC')
+        _RPC.clear()
 
-        rpc = Presence(client_id)
-        rpc.connect()
-        rpc.clear()  # Will throw InvalidID if client ID invalid
 
-        self._rpc = rpc
-        self._connected = True
+def is_connected():
+    return _CONNECTED
 
-    def stop(self):
-        self._rpc.clear()
-        self._rpc.close()
-        self._connected = False
 
-    def update(self, track):
-        if track:
-            position = int(track['trackPosition'])
-            position_minutes, position_seconds = divmod(position, 60)
+def connect():
+    if is_connected():
+        return
 
-            duration = int(track['trackDuration'])
-            duration_minutes, duration_seconds = divmod(duration, 60)
+    global _RPC
+    global _CONNECTED
 
-            details = f"{track['trackArtist']} - {track['trackName']}"
-            state = '%02d:%02d/%02d:%02d' % (position_minutes, position_seconds, duration_minutes, duration_seconds)
+    client_id = config.get('RPC', 'clientid', fallback=None)
 
-            self._rpc.update(state=state, details=details, large_image='icon', large_text=details)
-        else:
-            self._rpc.clear()
+    rpc = Presence(client_id)
+    rpc.connect()
+
+    _RPC = rpc
+    _CONNECTED = True
+
+    logger.info('RPC connected')
+
+
+def stop():
+    if not is_connected():
+        return
+
+    global _CONNECTED
+
+    _RPC.clear()
+    _RPC.close()
+    _CONNECTED = False
+
+    logger.info('RPC disconnected')
